df085e84dbcceb9bfd0218b5f76af31cc7fdac84
gabriel <gabriel@172.80.4.153>
2017-10-10 14:20:46 -0300

    Criadas entidades JPA

---
+++ WebApp/src/java/com/webapp/entidade/Hotel.java
@@ -0,0 +1,62 @@
+package com.webapp.entidade;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hotel implements Serializable {
+    
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Hotel() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Hotel other = (Hotel) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
---
+++ WebApp/src/java/com/webapp/entidade/Quarto.java
@@ -0,0 +1,77 @@
+package com.webapp.entidade;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Quarto implements Serializable {
+    
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Temporal (TemporalType.DATE)
+    private Date entrada;
+    
+    @Temporal (TemporalType.DATE)
+    private Date saida;
+
+    public Quarto() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Date entrada) {
+        this.entrada = entrada;
+    }
+
+    public Date getSaida() {
+        return saida;
+    }
+
+    public void setSaida(Date saida) {
+        this.saida = saida;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Quarto other = (Quarto) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
